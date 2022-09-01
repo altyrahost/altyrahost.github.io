d11d5b19ca4687af50b9bdfa5870940be83185f9
Nit <66027553+Nitlix@users.noreply.github.com>
2022-09-01 21:54:24 +0200

    e

--- wrapper.js
+++ wrapper.js
@@ -17,37 +17,48 @@ var changed = 0;
 var mobile = detectMob();
 
 
+function disableAnimations(){
+    $("*:not(.preloader)").css({
+        "transition-duration": "0s"});
+}
 
+function enableAnimations(delay=100){
+    setTimeout(function(){
+        $("*:not(.preloader)").css({
+            "transition-duration": ".5s"});
+    },delay)
+}
 
 function nitChangeScreen(smallerpls = false){
     if (mobile){
         if (changed != 1)
         {
             changed += 1;
-            $("*:not(.preloader)").css({
-                "transition-duration": "0s"});
-            document.documentElement.style.fontSize = 16*(window.innerHeight/1080*1.35) + "px";
-            setTimeout(function(){
-                $("*:not(.preloader)").css({
-                    "transition-duration": ".5s"});
-            },100)
+            disableAnimations()
+            document.documentElement.style.fontSize = 16*(window.innerHeight/1080*0.8) + "px";
+            enableAnimations()
             return 16*(window.innerHeight/1080*1.35);   
         }
         
     }
     else {
         if (window.innerHeight >= 700){
-        
+            disableAnimations()
             document.documentElement.style.fontSize = 16*(window.innerHeight/1080*1.75) + "px";
+            enableAnimations()
             return 16*(window.innerHeight/1080*1.75);
         }
         else {
             if (window.innerHeight <= 300){
+                disableAnimations()
                 document.documentElement.style.fontSize = 16*(window.innerHeight/1080*1.75) + "px";
+                enableAnimations()
                 return 16*(window.innerHeight/1080*1.75);
             }
             else {
+                disableAnimations()
                 document.documentElement.style.fontSize = "16px";
+                enableAnimations()
                 return 16;
             }
         }
